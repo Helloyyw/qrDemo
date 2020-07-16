a282e85bc9ccc474a969f0c24c701d20726e4cd2
Helloyyw <1475556814@qq.com>
2020-07-16 09:48:10 +0800

    Java 获取二维码Demo

---
+++ src/main/java/com/yyw/qrdemo/QrdemoApplication.java
@@ -0,0 +1,13 @@
+package com.yyw.qrdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrdemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yyw/qrdemo/Util/QRCodeUtil.java
@@ -0,0 +1,84 @@
+package com.yyw.qrdemo.Util;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Hashtable;
+
+public class QRCodeUtil {
+
+    private static final String CHARSET = "utf-8";
+
+    // 二维码尺寸
+    private static final int QRCODE_SIZE = 300;
+
+    public static BufferedImage createImage(String content) {
+        Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>();
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        hints.put(EncodeHintType.CHARACTER_SET, CHARSET);
+        hints.put(EncodeHintType.MARGIN, 1);
+        BitMatrix bitMatrix = null;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, QRCODE_SIZE, QRCODE_SIZE,
+                    hints);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        int width = bitMatrix.getWidth();
+        int height = bitMatrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, bitMatrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF);
+            }
+        }
+        return image;
+    }
+    /**
+     * BufferedImage 编码转换为 base64
+     * @param bufferedImage
+     * @return
+     */
+    public static String BufferedImageToBase64(BufferedImage bufferedImage) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();//io流
+        try {
+            ImageIO.write(bufferedImage, "png", baos);//写入流中
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        byte[] bytes = baos.toByteArray();//转换成字节
+        BASE64Encoder encoder = new BASE64Encoder();
+        String png_base64 = encoder.encodeBuffer(bytes).trim();//转换成base64串
+        png_base64 = png_base64.replaceAll("\n", "").replaceAll("\r", "");//删除 \r\n
+        System.out.println("值为：" + "data:image/jpg;base64," + png_base64);
+        return "data:image/jpg;base64," + png_base64;
+    }
+
+    /**
+     * base64 编码转换为 BufferedImage
+     * @param base64
+     * @return
+     */
+    public  static BufferedImage base64ToBufferedImage(String base64) {
+        BASE64Decoder decoder = new sun.misc.BASE64Decoder();
+        try {
+            byte[] bytes1 = decoder.decodeBuffer(base64);
+            ByteArrayInputStream bais = new ByteArrayInputStream(bytes1);
+            return ImageIO.read(bais);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
